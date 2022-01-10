46d5ad68902d3287e1b2b6f4080e3a6efffadc19
Ashish4321-hub <anmol2001anu@gmail.com>
2022-01-10 18:31:40 +0530

    commit add location

---
+++ loc.js
@@ -0,0 +1,60 @@
+/*const successCallback =(position)=> {
+  document.querySelector(".city").innerHTML=position;
+ //console.log(position);
+};
+const errorCallback = (error) => {
+console.console.error(error);
+};
+navigator.geolocation.getCurrentPosition(successCallback,errorCallback,{enableHighAccuracy:true,timeout:5000});*/
+/////
+// Step 1: Get user coordinates
+function getCoordintes() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(pos) {
+		var crd = pos.coords;
+		var lat = crd.latitude.toString();
+		var lng = crd.longitude.toString();
+		var coordinates = [lat, lng];
+		console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+		getCity(coordinates);
+		return;
+
+	}
+
+	function error(err) {
+		console.warn(`ERROR(${err.code}): ${err.message}`);
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+// Step 2: Get city name
+function getCity(coordinates) {
+	var xhr = new XMLHttpRequest();
+	var lat = coordinates[0];
+	var lng = coordinates[1];
+
+	// Paste your LocationIQ token below.
+	xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.4f60336a9e0d3a374efab0b0923576ee&lat=" +
+	lat + "&lon=" + lng + "&format=json", true);
+	xhr.send();
+	xhr.onreadystatechange = processRequest;
+	xhr.addEventListener("readystatechange", processRequest, false);
+
+	function processRequest(e) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var response = JSON.parse(xhr.responseText);
+			var city = response.address.city;
+			//console.log(city);
+      document.querySelector(".city").innerHTML=city;
+			return;
+		}
+	}
+}
+
+getCoordintes();
